cc1d9b24e5677cb7bb26c60896a362456e02948b
myfreeweb <me@myfreeweb.ru>
2011-10-13 01:29:14 +0400

    2 space indentation install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 link() {
-    rm $HOME/.$1
-    ln -s $(pwd)/$1 $HOME/.$1
+  rm $HOME/.$1
+  ln -s $(pwd)/$1 $HOME/.$1
 }
 link_custom() {
   rm $2
@@ -35,15 +35,15 @@ link_custom re.pl $HOME/.re.pl/repl.rc
 echo "vim/tmp vim/tmp/undo vim/tmp/swap vim/tmp/backups" | xargs mkdir -p
 
 if [[ $(uname) = 'Linux' ]]; then
-    QUAKE=$HOME/.quakelive/quakelive/home/baseq3
+  QUAKE=$HOME/.quakelive/quakelive/home/baseq3
 elif [[ $(uname) = 'Darwin' ]]; then
-    cp com.floatboth.arrrsync.plist ~/Library/LaunchAgents/
-    defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
-    echo "0x08000100:0" > ~/.CFUserTextEncoding
-    echo 'do shell script "'$(pwd)'/bin/newpost.sh"' > ~/Library/Application\ Support/LaunchBar/Actions/New\ post.scpt
-    QUAKE=$HOME/Library/Application\ Support/QuakeLive/quakelive/home/baseq3
+  cp com.floatboth.arrrsync.plist ~/Library/LaunchAgents/
+  defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+  echo "0x08000100:0" > ~/.CFUserTextEncoding
+  echo 'do shell script "'$(pwd)'/bin/newpost.sh"' > ~/Library/Application\ Support/LaunchBar/Actions/New\ post.scpt
+  QUAKE=$HOME/Library/Application\ Support/QuakeLive/quakelive/home/baseq3
 fi
 if [[ -d $QUAKE ]]; then
-    link_custom quakelive.cfg $QUAKE/quakelive.cfg
-    echo 'exec "quakelive.cfg"' > $QUAKE/autoexec.cfg
+  link_custom quakelive.cfg $QUAKE/quakelive.cfg
+  echo 'exec "quakelive.cfg"' > $QUAKE/autoexec.cfg
 fi
