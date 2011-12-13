02d7bebe5df5fadcaf8b3e6804f30a54cef35df7
myfreeweb <floatboth@me.com>
2011-12-13 17:43:40 +0400

    backup update cache

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 These are my dotfiles. I honestly didn't know what to put there, so I wrote a pre-commit hook which inserts a random Jon Lajoie lyric. Here it is:
 
-> Other rappers are comprehensible, not me
-> First on the mic MC Confusing!
+> MC Confusing back in this, bitch  
+> With a parking sandwich and a chicken ticket
--- bin/backup.sh
+++ bin/backup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env zsh
+BACKUP_DIR=$HOME/.backup_cache
+mkdir -p $BACKUP_DIR
+cachepath() {
+  echo $BACKUP_DIR/$(echo $1 | tr "/ " "__")
+}
+cache_get() {
+  CPATH=$(cachepath $1)
+  [[ -e $CPATH ]] && cat $CPATH
+}
 backup() {
   FILES=$(echo $argv | sed "s/^[^ ]* //")
   tarsnap -c -f $1-$(date +%Y-%m-%d-%H:%M) $FILES
@@ -7,12 +16,13 @@ backup_file() {
   [[ -e $1 ]] && backup $(basename $1) $1
 }
 backup_dir() {
-  [[ -d $2 ]] && backup $1 $2
+  [[ -d $2 ]] && [[ $(cache_get $2) != $(ls -la $2) ]] && backup $1 $2 && ls -la $2 > $(cachepath $2)
 }
 backup_last_file() {
-  [[ -d $2 ]] && backup $1 $2/$(ls -t $2 | head -n 1)
+  FILE=$(ls -t $2 | head -n 1)
+  [[ -d $2 ]] && [[ $(cache_get $2) != $FILE ]] && backup $1 $2/$FILE && echo $FILE > $(cachepath $2)
 }
-APPSUP=$HOME/Library/Application\ Support/
+APPSUP=$HOME/Library/Application\ Support
 
 backup_file $HOME/Documents/my.ledger
 backup_dir private-code $CODEDIR/private
