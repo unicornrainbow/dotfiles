2ba6e9d9b70d8e893da4e498346e52cd796aaa2a
myfreeweb <floatboth@me.com>
2011-12-18 15:57:56 +0400

    install update

--- install.sh
+++ install.sh
@@ -14,14 +14,12 @@ link ackrc
 link zshrc
 link hgrc
 link ctags
-link conkerorrc
 link gitconfig
 link teamocil
 link tmux.conf
 link vim
 link vimrc
 link gvimrc
-link js
 link irbrc
 link inputrc
 link nethackrc
@@ -35,12 +33,11 @@ link_custom ssh_config $HOME/.ssh/config
 mkdir -p $HOME/.re.pl
 link_custom re.pl $HOME/.re.pl/repl.rc
 
-echo "vim/tmp vim/tmp/undo vim/tmp/swap vim/tmp/backups" | xargs mkdir -p
+echo "vim/tmp vim/tmp/undo vim/tmp/backups" | xargs mkdir -p
 
 if [[ $(uname) = 'Linux' ]]; then
   cat ./crontab | crontab
   QUAKE=$HOME/.quakelive/quakelive/home/baseq3
-  SUBLIME=$HOME/.config/sublime-text-2
 elif [[ $(uname) = 'Darwin' ]]; then
   cat ./crontab ./crontab_mac | crontab
   chflags nohidden ~/Library
@@ -55,10 +52,8 @@ elif [[ $(uname) = 'Darwin' ]]; then
   echo "0x08000100:0" > $HOME/.CFUserTextEncoding
   link_custom evernote_selection.plist $HOME/Library/LaunchAgents/com.floatboth.evernote_selection.plist
   QUAKE=$HOME/Library/Application\ Support/QuakeLive/quakelive/home/baseq3
-  SUBLIME=$HOME/Library/Application\ Support/Sublime\ Text\ 2
 fi
 if [[ -d $QUAKE ]]; then
   link_custom quakelive.cfg $QUAKE/quakelive.cfg
   echo 'exec "quakelive.cfg"' > $QUAKE/autoexec.cfg
 fi
-[[ -d $SUBLIME ]] && link_custom sublime $SUBLIME/Packages/Floatboth
